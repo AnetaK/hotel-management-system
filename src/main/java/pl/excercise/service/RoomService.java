99bb530905d0c2a4f62e78626966e9e702f89001
AnetaK <karbowska.aneta@gmail.com>
2016-10-03 22:21:28 +0200

    jpa query

--- src/main/java/pl/excercise/service/RoomService.java
+++ src/main/java/pl/excercise/service/RoomService.java
@@ -3,12 +3,14 @@ package pl.excercise.service;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
 import pl.excercise.model.room.ParametrizedRoom;
 import pl.excercise.model.room.RoomEntity;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -33,36 +35,71 @@ public class RoomService {
         LOGGER.trace("Searching in DB rooms for parameters" + parametrizedRoom.toString());
 
         DaysCount daysCount = new DaysCount();
-        LocalDate localDate = LocalDate.parse(parametrizedRoom.getAvailableTo()).plusDays(1);
+        LocalDate localDate = LocalDate.parse(parametrizedRoom.getAvailableTo());
         List<String> datesRange = daysCount.returnDaysList(parametrizedRoom.getAvailableFrom(), localDate.toString());
 
         System.out.println("datesRange = " + datesRange.toString());
 
-
-//        List<RoomEntity> rooms = em.createNativeQuery(" select distinct r.id, r.roomType, r.windowsExposure " +
-//                        "from  RoomEntity r, RoomEntity_bookedDates b " +
-//                        "where b.bookedDates not in :datesRange  " +
-//                        "and r.id = b.RoomEntity_id " +
-//                        "and r.roomType = :roomType " +
-//                        "and r.windowsExposure = :windowsExposure " +
-//                        "order by r.id "
-//                , RoomEntity.class)
+//        List<RoomEntity> rooms = em.createQuery(" select distinct r.id, r.roomType, r.windowsExposure " +
+//                "from  RoomEntity r left join r.bookedDates b " +
+//                "where b not in :datesRange  " +
+//                "and r.roomType = :roomType " +
+//                "and r.windowsExposure = :windowsExposure " +
+//                "order by r.id "
+//        )
 //                .setParameter("roomType", parametrizedRoom.getRoomType())
 //                .setParameter("windowsExposure", parametrizedRoom.getWindowsExposure())
 //                .setParameter("datesRange", datesRange)
 //                .getResultList();
 
-        List<RoomEntity> rooms = em.createQuery(" select distinct r.id, r.roomType, r.windowsExposure " +
+        List<String> datesRange1 = em.createQuery(" select distinct b from RoomEntity r left join r.bookedDates b where b not in :datesRange").setParameter("datesRange", datesRange)
+                .getResultList();
+        System.out.println("datesRange1.toString() = " + datesRange1.toString());
+
+        String query = " ";
+        for (String date :
+                datesRange) {
+            query = query + "\'" + date + "\' member of b and ";
+        }
+
+
+        List<RoomEntity> rooms2 = em.createQuery(" select distinct r from RoomEntity r  join r.bookedDates b where  :dummy not member of b and :dummy1 not member of b  ")
+                .setParameter("dummy", "2016-05-10")
+                .setParameter("dummy1", "2016-05-11")
+                .getResultList();
+        System.out.println("rooms2.toString() = " + rooms2.toString());
+
+        if (query.endsWith("and ")) {
+            query = query.substring(0, query.lastIndexOf("and") - 1) + " \'1\' = ";
+        }
+        System.out.println("query" + query);
+        Query query1 = em.createQuery(query);
+        Query query2 = em.createQuery(" select distinct r.id, r.roomType, r.windowsExposure " +
                 "from  RoomEntity r left join r.bookedDates b " +
-                "where b not in :datesRange  " +
-                "and r.roomType = :roomType " +
+                "where " +
+                " r.roomType = :roomType " +
                 "and r.windowsExposure = :windowsExposure " +
-                "order by r.id "
-                 )
+                "and :query = \'1\' " +
+                " order by r.id "
+        )
                 .setParameter("roomType", parametrizedRoom.getRoomType())
                 .setParameter("windowsExposure", parametrizedRoom.getWindowsExposure())
-                .setParameter("datesRange", datesRange)
+                .setParameter("query", query1);
+
+        List<RoomEntity> rooms = em.createQuery(" select distinct r.id, r.roomType, r.windowsExposure " +
+                "from  RoomEntity r left join r.bookedDates b " +
+                "where " +
+                " r.roomType = :roomType " +
+                "and r.windowsExposure = :windowsExposure " +
+                "and :query = \'1\' " +
+                " order by r.id "
+        )
+                .setParameter("roomType", parametrizedRoom.getRoomType())
+                .setParameter("windowsExposure", parametrizedRoom.getWindowsExposure())
+                .setParameter("query", query)
                 .getResultList();
+
+        System.out.println("datesRange1 = " + datesRange1.toString());
 
         LOGGER.debug("Number of rooms that meet the conditions: " + rooms.size());
         LOGGER.trace("Found rooms: " + rooms.toString());
